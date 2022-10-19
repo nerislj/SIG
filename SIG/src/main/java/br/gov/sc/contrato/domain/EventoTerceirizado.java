e2b16d3a740057a15b174d2368eb67c27218b138
nerijunior <nerijunior@DETRANSC-005943.detransc.sc.gov.br>
2022-10-19 13:46:57 -0300

    controle contrato geapo

--- SIG/src/main/java/br/gov/sc/contrato/domain/EventoTerceirizado.java
+++ SIG/src/main/java/br/gov/sc/contrato/domain/EventoTerceirizado.java
@@ -14,155 +14,171 @@ import br.gov.sc.sgi.domain.Usuario;
 
 @SuppressWarnings("serial")
 @Entity
-@Table(
-   name = "controle_eventoterceirizado"
-)
+@Table(name = "controle_eventoterceirizado")
 public class EventoTerceirizado extends GenericDomainContrato {
-   @Column(
-      length = 20
-   )
-   private String tipoEvento;
-   @OneToOne
-   @JoinColumn(
-      nullable = false
-   )
-   private FuncionarioTerceirizado colaborador;
-   @Temporal(TemporalType.DATE)
-   @Column
-   private Date dataEventoInicial;
-   @Temporal(TemporalType.DATE)
-   @Column
-   private Date dataEventoFinal;
-   @OneToOne
-   @JoinColumn
-   private FuncionarioTerceirizado substituto;
-   @Temporal(TemporalType.DATE)
-   @Column
-   private Date dataSubstitutoInicial;
-   @Temporal(TemporalType.DATE)
-   @Column
-   private Date dataSubstitutoFinal;
-   @Column(
-      length = 30
-   )
-   private Integer dias;
-   @Temporal(TemporalType.DATE)
-   @Column(
-      nullable = false
-   )
-   private Date dataCadastro;
-   @OneToOne
-   @JoinColumn(
-      nullable = false
-   )
-   private EmpresaTerceirizada empresaTerceirizada;
-   @OneToOne
-   @JoinColumn(
-      nullable = false
-   )
-   private Usuario usuarioCadastro;
-   
-   @OneToOne
-   @JoinColumn(
-      nullable = false
-   )
-   private ContratoTerceirizado contratoTerceirizado;
-   
-   
+	@Column(length = 20)
+	private String tipoEvento;
+	@OneToOne
+	@JoinColumn(nullable = false)
+	private FuncionarioTerceirizado colaborador;
+	@Temporal(TemporalType.DATE)
+	@Column
+	private Date dataEventoInicial;
+	@Temporal(TemporalType.DATE)
+	@Column
+	private Date dataEventoFinal;
+	@OneToOne
+	@JoinColumn
+	private FuncionarioTerceirizado substituto;
+	@Temporal(TemporalType.DATE)
+	@Column
+	private Date dataSubstitutoInicial;
+	@Temporal(TemporalType.DATE)
+	@Column
+	private Date dataSubstitutoFinal;
+	@Column(length = 30)
+	private Integer dias;
+	@Temporal(TemporalType.DATE)
+	@Column(nullable = false)
+	private Date dataCadastro;
+	@OneToOne
+	@JoinColumn(nullable = false)
+	private EmpresaTerceirizada empresaTerceirizada;
+	@OneToOne
+	@JoinColumn(nullable = false)
+	private Usuario usuarioCadastro;
 
-   public ContratoTerceirizado getContratoTerceirizado() {
-	return contratoTerceirizado;
+	@OneToOne
+	@JoinColumn(nullable = false)
+	private ContratoTerceirizado contratoTerceirizado;
+
+	@Column(length = 255)
+	private String motivo;
+
+	@Column(length = 255)
+	private String horas;
+
+	
+	
+	
+
+
+
+	public String getMotivo() {
+		return motivo;
+	}
+
+	public void setMotivo(String motivo) {
+		this.motivo = motivo;
+	}
+
+	public String getHoras() {
+		return horas;
+	}
+
+	public void setHoras(String horas) {
+		this.horas = horas;
+	}
+
+	public ContratoTerceirizado getContratoTerceirizado() {
+		return contratoTerceirizado;
+	}
+
+	public void setContratoTerceirizado(ContratoTerceirizado contratoTerceirizado) {
+		this.contratoTerceirizado = contratoTerceirizado;
+	}
+
+	public Usuario getUsuarioCadastro() {
+		return usuarioCadastro;
+	}
+
+	public void setUsuarioCadastro(Usuario usuarioCadastro) {
+		this.usuarioCadastro = usuarioCadastro;
+	}
+
+	public EmpresaTerceirizada getEmpresaTerceirizada() {
+		return this.empresaTerceirizada;
+	}
+
+	public void setEmpresaTerceirizada(EmpresaTerceirizada empresaTerceirizada) {
+		this.empresaTerceirizada = empresaTerceirizada;
+	}
+
+	public String getTipoEvento() {
+
+		return this.tipoEvento;
+	}
+
+	public FuncionarioTerceirizado getColaborador() {
+		return this.colaborador;
+	}
+
+	public void setColaborador(FuncionarioTerceirizado colaborador) {
+		this.colaborador = colaborador;
+	}
+
+	public FuncionarioTerceirizado getSubstituto() {
+		return this.substituto;
+	}
+
+	public void setSubstituto(FuncionarioTerceirizado substituto) {
+		this.substituto = substituto;
+	}
+
+	public Integer getDias() {
+		return this.dias;
+	}
+
+	public void setDias(Integer dias) {
+		this.dias = dias;
+	}
+
+	public Date getDataCadastro() {
+		return this.dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public void setTipoEvento(String tipoEvento) {
+		this.tipoEvento = tipoEvento;
+	}
+
+	public Date getDataEventoInicial() {
+		return this.dataEventoInicial;
+	}
+
+	public void setDataEventoInicial(Date dataEventoInicial) {
+		this.dataEventoInicial = dataEventoInicial;
+	}
+
+	public Date getDataEventoFinal() {
+		return this.dataEventoFinal;
+	}
+
+	public void setDataEventoFinal(Date dataEventoFinal) {
+		this.dataEventoFinal = dataEventoFinal;
+	}
+
+	public Date getDataSubstitutoInicial() {
+		return this.dataSubstitutoInicial;
+	}
+
+	public void setDataSubstitutoInicial(Date dataSubstitutoInicial) {
+		this.dataSubstitutoInicial = dataSubstitutoInicial;
+	}
+
+	public Date getDataSubstitutoFinal() {
+		return this.dataSubstitutoFinal;
+	}
+
+	public void setDataSubstitutoFinal(Date dataSubstitutoFinal) {
+		this.dataSubstitutoFinal = dataSubstitutoFinal;
+	}
+	
+	
+	
+	
 }
 
-public void setContratoTerceirizado(ContratoTerceirizado contratoTerceirizado) {
-	this.contratoTerceirizado = contratoTerceirizado;
-}
-
-public Usuario getUsuarioCadastro() {
-	return usuarioCadastro;
-}
-
-public void setUsuarioCadastro(Usuario usuarioCadastro) {
-	this.usuarioCadastro = usuarioCadastro;
-}
-
-public EmpresaTerceirizada getEmpresaTerceirizada() {
-      return this.empresaTerceirizada;
-   }
-
-   public void setEmpresaTerceirizada(EmpresaTerceirizada empresaTerceirizada) {
-      this.empresaTerceirizada = empresaTerceirizada;
-   }
-
-   public String getTipoEvento() {
-      return this.tipoEvento;
-   }
-
-   public FuncionarioTerceirizado getColaborador() {
-      return this.colaborador;
-   }
-
-   public void setColaborador(FuncionarioTerceirizado colaborador) {
-      this.colaborador = colaborador;
-   }
-
-   public FuncionarioTerceirizado getSubstituto() {
-      return this.substituto;
-   }
-
-   public void setSubstituto(FuncionarioTerceirizado substituto) {
-      this.substituto = substituto;
-   }
-
-   public Integer getDias() {
-      return this.dias;
-   }
-
-   public void setDias(Integer dias) {
-      this.dias = dias;
-   }
-
-   public Date getDataCadastro() {
-      return this.dataCadastro;
-   }
-
-   public void setDataCadastro(Date dataCadastro) {
-      this.dataCadastro = dataCadastro;
-   }
-
-   public void setTipoEvento(String tipoEvento) {
-      this.tipoEvento = tipoEvento;
-   }
-
-   public Date getDataEventoInicial() {
-      return this.dataEventoInicial;
-   }
-
-   public void setDataEventoInicial(Date dataEventoInicial) {
-      this.dataEventoInicial = dataEventoInicial;
-   }
-
-   public Date getDataEventoFinal() {
-      return this.dataEventoFinal;
-   }
-
-   public void setDataEventoFinal(Date dataEventoFinal) {
-      this.dataEventoFinal = dataEventoFinal;
-   }
-
-   public Date getDataSubstitutoInicial() {
-      return this.dataSubstitutoInicial;
-   }
-
-   public void setDataSubstitutoInicial(Date dataSubstitutoInicial) {
-      this.dataSubstitutoInicial = dataSubstitutoInicial;
-   }
-
-   public Date getDataSubstitutoFinal() {
-      return this.dataSubstitutoFinal;
-   }
-
-   public void setDataSubstitutoFinal(Date dataSubstitutoFinal) {
-      this.dataSubstitutoFinal = dataSubstitutoFinal;
-   }
-}
--- SIG/src/main/java/br/gov/sc/contrato/domain/FuncionarioTerceirizado.java
+++ SIG/src/main/java/br/gov/sc/contrato/domain/FuncionarioTerceirizado.java
@@ -22,94 +22,85 @@ import org.hibernate.annotations.FetchMode;
 
 @SuppressWarnings("serial")
 @Entity
-@Table(
-   name = "controle_funcionarioterceirizado"
-)
+@Table(name = "controle_funcionarioterceirizado")
 public class FuncionarioTerceirizado extends GenericDomainContrato {
-   @OneToOne
-   @JoinColumn(
-      unique = true
-   )
-   private EventoTerceirizado tipoEvento;
-   @OneToOne
-   @JoinColumn(
-      nullable = false,
-      unique = true
-   )
-   private PessoaFisica pessoa;
-   @ManyToOne
-   @JoinColumn
-   private Unidade unidade;
-   
-   @Temporal(TemporalType.DATE)
-   @Column(
-      nullable = false
-   )
-   private Date dataCadastro;
-   @OneToOne
-   @JoinColumn(
-      nullable = false
-   )
-   private CargoTerceirizado cargoTerceirizado;
-   @OneToOne
-   @JoinColumn(
-      nullable = false
-   )
-   private Usuario usuarioCadastro;
-   @OneToMany(
-      fetch = FetchType.LAZY,
-      mappedBy = "funcionarioTerceirizado"
-   )
-   @Fetch(FetchMode.SUBSELECT)
-   private List<ContratoRelacao> contratoRelacao;
+	@OneToOne
+	@JoinColumn(unique = true)
+	private EventoTerceirizado tipoEvento;
+	@OneToOne
+	@JoinColumn(nullable = false, unique = true)
+	private PessoaFisica pessoa;
+	@ManyToOne
+	@JoinColumn
+	private Unidade unidade;
+	@ManyToOne
+	@JoinColumn(nullable = true)
+	private Setor setor;
+	@Temporal(TemporalType.DATE)
+	@Column(nullable = false)
+	private Date dataCadastro;
+	@OneToOne
+	@JoinColumn(nullable = false)
+	private CargoTerceirizado cargoTerceirizado;
+	@OneToOne
+	@JoinColumn(nullable = false)
+	private Usuario usuarioCadastro;
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "funcionarioTerceirizado")
+	@Fetch(FetchMode.SUBSELECT)
+	private List<ContratoRelacao> contratoRelacao;
 
-   public EventoTerceirizado getTipoEvento() {
-      return this.tipoEvento;
-   }
+	public EventoTerceirizado getTipoEvento() {
+		return this.tipoEvento;
+	}
 
-   public void setTipoEvento(EventoTerceirizado tipoEvento) {
-      this.tipoEvento = tipoEvento;
-   }
+	public void setTipoEvento(EventoTerceirizado tipoEvento) {
+		this.tipoEvento = tipoEvento;
+	}
 
-   public PessoaFisica getPessoa() {
-      return this.pessoa;
-   }
+	public PessoaFisica getPessoa() {
+		return this.pessoa;
+	}
 
-   public void setPessoa(PessoaFisica pessoa) {
-      this.pessoa = pessoa;
-   }
+	public void setPessoa(PessoaFisica pessoa) {
+		this.pessoa = pessoa;
+	}
 
-   public Unidade getUnidade() {
-      return this.unidade;
-   }
+	public Unidade getUnidade() {
+		return this.unidade;
+	}
 
-   public void setUnidade(Unidade unidade) {
-      this.unidade = unidade;
-   }
+	public void setUnidade(Unidade unidade) {
+		this.unidade = unidade;
+	}
+	public Setor getSetor() {
+		return setor;
+	}
 
-  
+	public void setSetor(Setor setor) {
+		this.setor = setor;
+	}
 
-   public Date getDataCadastro() {
-      return this.dataCadastro;
-   }
+	public Date getDataCadastro() {
+		return this.dataCadastro;
+	}
 
-   public void setDataCadastro(Date dataCadastro) {
-      this.dataCadastro = dataCadastro;
-   }
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
 
-   public CargoTerceirizado getCargoTerceirizado() {
-      return this.cargoTerceirizado;
-   }
+	public CargoTerceirizado getCargoTerceirizado() {
+		return this.cargoTerceirizado;
+	}
 
-   public void setCargoTerceirizado(CargoTerceirizado cargoTerceirizado) {
-      this.cargoTerceirizado = cargoTerceirizado;
-   }
+	public void setCargoTerceirizado(CargoTerceirizado cargoTerceirizado) {
+		this.cargoTerceirizado = cargoTerceirizado;
+	}
 
-   public Usuario getUsuarioCadastro() {
-      return this.usuarioCadastro;
-   }
+	public Usuario getUsuarioCadastro() {
+		return this.usuarioCadastro;
+	}
 
-   public void setUsuarioCadastro(Usuario usuarioCadastro) {
-      this.usuarioCadastro = usuarioCadastro;
-   }
+	public void setUsuarioCadastro(Usuario usuarioCadastro) {
+		this.usuarioCadastro = usuarioCadastro;
+	}
 }
