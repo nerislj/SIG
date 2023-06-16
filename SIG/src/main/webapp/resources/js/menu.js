ece5b837b64e7d5858e168442a2a9e69fb6d38a1
nerijunior <nerijunior@DETRANSC-005943.detransc.sc.gov.br>
2023-06-16 09:14:09 -0300

    menu js

--- SIG/src/main/webapp/resources/js/menu.js
+++ SIG/src/main/webapp/resources/js/menu.js
@@ -1,24 +1,4 @@
-<<<<<<< HEAD
-$(document).ready(function() {
-	$('.menu li:has(ul)').click(function(e) {
-		e.preventDefault();
 
-		if($(this).hasClass('activado')) {
-			$(this).removeClass('activado');
-			$(this).children('ul').slideUp();
-		} else {
-			$('.menu li ul').slideUp();
-			$('.menu li').removeClass('activado');
-			$(this).addClass('activado');
-			$(this).children('ul').slideDown();
-		}
-
-		$('.menu li ul li a').click(function() {
-			window.location.href = $(this).attr('href');
-		})
-	});
-});
-=======
 $(document).ready(function() {
 	$('.menu li:has(ul)').click(function(e) {
 		e.preventDefault();
@@ -39,5 +19,25 @@ $(document).ready(function() {
 	});
 });
 
+$(document).ready(function() {
+	$('.menu li:has(ul)').click(function(e) {
+		e.preventDefault();
 
->>>>>>> branch 'main' of https://github.com/nerislj/SIG.git
+		if($(this).hasClass('activado')) {
+			$(this).removeClass('activado');
+			$(this).children('ul').slideUp();
+		} else {
+			$('.menu li ul').slideUp();
+			$('.menu li').removeClass('activado');
+			$(this).addClass('activado');
+			$(this).children('ul').slideDown();
+		}
+
+		$('.menu li ul li a').click(function() {
+			window.location.href = $(this).attr('href');
+		})
+	});
+});
+
+
+
