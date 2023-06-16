6d63a7b664d771efee80a7c4014049182db3b5d0
Neri Junior <neri_slj@hotmail.com>
2023-06-16 11:13:25 -0300

    Revert "menu js"
    This reverts commit ece5b837b64e7d5858e168442a2a9e69fb6d38a1.

--- SIG/src/main/webapp/resources/js/menu.js
+++ SIG/src/main/webapp/resources/js/menu.js
@@ -1,4 +1,24 @@
+<<<<<<< HEAD
+$(document).ready(function() {
+	$('.menu li:has(ul)').click(function(e) {
+		e.preventDefault();
 
+		if($(this).hasClass('activado')) {
+			$(this).removeClass('activado');
+			$(this).children('ul').slideUp();
+		} else {
+			$('.menu li ul').slideUp();
+			$('.menu li').removeClass('activado');
+			$(this).addClass('activado');
+			$(this).children('ul').slideDown();
+		}
+
+		$('.menu li ul li a').click(function() {
+			window.location.href = $(this).attr('href');
+		})
+	});
+});
+=======
 $(document).ready(function() {
 	$('.menu li:has(ul)').click(function(e) {
 		e.preventDefault();
@@ -19,25 +39,5 @@ $(document).ready(function() {
 	});
 });
 
-$(document).ready(function() {
-	$('.menu li:has(ul)').click(function(e) {
-		e.preventDefault();
 
-		if($(this).hasClass('activado')) {
-			$(this).removeClass('activado');
-			$(this).children('ul').slideUp();
-		} else {
-			$('.menu li ul').slideUp();
-			$('.menu li').removeClass('activado');
-			$(this).addClass('activado');
-			$(this).children('ul').slideDown();
-		}
-
-		$('.menu li ul li a').click(function() {
-			window.location.href = $(this).attr('href');
-		})
-	});
-});
-
-
-
+>>>>>>> branch 'main' of https://github.com/nerislj/SIG.git
